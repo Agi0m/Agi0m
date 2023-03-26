7dcedbc9f67ee0938429b62fdde7722dae3662a4
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-03-27 00:19:37 +0200

    Update Fresh-install.sh
    fixed some errors

--- Fresh-install.sh
+++ Fresh-install.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade
-apt install geany conky htop neofetch kitty cmatrix duf ranger xsensorsr rofi -y
+apt install geany conky htop neofetch kitty cmatrix duf ranger xsensors rofi -y
 -----------------------------------------------------------------------------------------------------------
 #flatpak install com.discordapp.Discord -y
 flatpak install com.github.tch84.Flatseal -y
@@ -16,8 +16,8 @@ flatpak install onfo.febvre.Komikku -y
 #flatpak install io.freetubeapp.FreeTube -y
 ------------------------------------------------------------------------------------------------------------
 
-wget https://github.com/xgi/houdoku/releases/download/v2.11.0/Houdoku-2.11.0.AppImage -y
-wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage -y
+wget https://github.com/xgi/houdoku/releases/download/v2.11.0/Houdoku-2.11.0.AppImage
+wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage
 
 apt autoclean
 apt autoremove
