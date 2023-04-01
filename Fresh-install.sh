603b39d7ae63a821f441414bb239966ace06dccf
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-04-01 19:03:23 +0200

    Fixed some stuff
    Fixed and added some stuff

--- Fresh-install.sh
+++ Fresh-install.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+echo "## Update and upgrade"
 
 sudo apt update
 sudo apt upgrade
-apt install geany conky htop neofetch kitty cmatrix duf ranger xsensors rofi plank -y
------------------------------------------------------------------------------------------------------------
+apt install conky htop neofetch kitty cmatrix duf ranger xsensors rofi plank dropbox nala -y
+
+echo "## Install deb-get"
+sudo apt install curl
+curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
+
+echo "## Install using deb-get"
+
+sudo deb-get update
+sudo deb-get install librewolf
+sudo deb-get install codium
+#sudo deb-get install jellyfin
+
+echo "-----------------------------------------------------------------------------------------------------------"
 #flatpak install com.discordapp.Discord -y
-flatpak install com.github.tch84.Flatseal -y
-flatpak install com.github.johnfactotum.Foliate -y
-flatpak install com.github.iwalton3.jellyfin-media-player -y
-flatpak install onfo.febvre.Komikku -y
+#flatpak install com.github.tch84.Flatseal -y
+#flatpak install com.github.johnfactotum.Foliate -y
+#flatpak install com.github.iwalton3.jellyfin-media-player -y
+#/flatpak install onfo.febvre.Komikku -y
 #flatpak install org.libreoffice.Libreoffice -y
 #flatpak install net.agalwood.Motrix -y
 #flatpak install org.mozilla.Thunderbird -y
@@ -19,9 +32,14 @@ flatpak install onfo.febvre.Komikku -y
 wget https://github.com/xgi/houdoku/releases/download/v2.11.0/Houdoku-2.11.0.AppImage
 wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage
 
+echo "## Install appimagelauncher"
+./-appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage install
+
 apt autoclean
 apt autoremove
 
-reboot
+echo "All done"
+
+#/reboot
 
 ##The end
---
+++ remove-after-fresh-install.sh
@@ -0,0 +1,49 @@
+## Remove apps after a fresh install
+
+## Thunderbird
+
+sudo apt purge thunderbird
+sudo apt purge thunderbird-gnome-support
+sudo apt purge thunderbird-locale-en
+sudo apt purge thunderbird-licale-en-us
+
+## Libreoffice
+
+sudo apt purge libreoffice-*
+sudo apt purge ure
+sudo apt purge ure-*
+sudo apt purge hunspell-*
+sudo apt purge mythes-*
+sudo apt purge hyphen-*
+sudo apt purge libuno-*
+
+## Drawing
+
+sudo apt purge drawing
+
+## Hexchat
+
+sudo apt purge hexchat
+sudo apt purge hexchat-common
+
+## Hypnotix
+
+sudo apt purge hypnotix
+
+## Rhythmbox
+
+sudo apt purge rhythmbox
+sudo apt purge rhythmbox-*
+
+## Transmission
+
+sudo apt purge transmission-*
+
+## Calculator
+
+sudo apt purge gnome-calculator
+
+## Firefox
+
+sudo apt purge firefox
+sudo apt purge firefox-*
