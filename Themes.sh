1c8facf565344537b9356381268e10473f98a37a
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-04-03 00:48:28 +0200

    Starting to work on themes, icons and cursors

---
+++ Themes.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+echo "##############################"
+echo "      Cloning themes          "
+echo "##############################"
+
+git clone https://github.com/EliverLara/Kripton
+git clone https://github.com/EliverLara/Nordic
+git clone https://github.com/EliverLara/otis
+git clone https://github.com/darkomarko42/Quartz-Night
+git clone https://github.com/vinceliuice/Graphite-gtk-theme
+git clone https://github.com/i-mint/bluesky
+git clone https://github.com/vinceliuice/Colloid-gtk-theme
+git clone https://github.com/vinceliuice/Nephrite-gtk-theme
+git clone https://github.com/i-mint/LightningBug
+git clone https://github.com/vinceliuice/Lavanda-gtk-theme
+git clone https://github.com/erenyldz89/Marshmallow1
+git clone https://github.com/EliverLara/Sweet
+git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme
+git clone https://github.com/ZorinOS/zorin-desktop-themes
+
+
+echo "##################################################"
+echo "  Starting to extract themes at /home/user/.themes"
+echo "##################################################"
