25becc8ca38cea528d28be004d0fcab928a6f52c
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-04-03 22:18:45 +0200

    remove all white themes on .themes folder

---
+++ remove-white-themes.sh
@@ -0,0 +1,16 @@
+echo "                               "
+echo "                               "
+
+echo "############################################"
+echo " Since I am a drak theme fan I want to
+     remove all white themes from my .themes folder"
+echo "############################################"
+
+
+cd ~/.themes
+rm -rf light* Light*
+
+echo "                                     "
+echo "                                     "
+echo "                                     "
+echo " All done"
