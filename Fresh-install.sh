cead3012a6926c0e88c6d4851964ae8597ada951
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-04-04 20:46:09 +0200

    Themes worked ( Updated fresh install)

--- Fresh-install.sh
+++ Fresh-install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-echo "## Update and upgrade"
+echo " Update and upgrade "
 
 sudo apt update
 sudo apt upgrade -y
-apt install conky htop neofetch kitty cmatrix duf ranger xsensors rofi plank dropbox nala variety zsh -y
+apt install conky htop neofetch kitty duf ranger xsensors rofi plank dropbox nala variety zsh libsass1 -y
 
-echo "## Installing deb-get"
+echo " Installing deb-get "
 sudo apt install curl
 curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
 
-echo "## Installing using deb-get"
+echo " Installing using deb-get "
 
 sudo deb-get update
 sudo deb-get install librewolf
 sudo deb-get install codium
 #sudo deb-get install jellyfin
 
-echo " Installing Flatpak (Unchechk if you want to install these "
+echo " Installing Flatpak (Unchechk if you want to install these) "
 #flatpak install com.discordapp.Discord -y
 #flatpak install com.github.tch84.Flatseal -y
 #flatpak install com.github.johnfactotum.Foliate -y
@@ -29,8 +29,6 @@ echo " Installing Flatpak (Unchechk if you want to install these "
 #flatpak install io.freetubeapp.FreeTube -y
 ------------------------------------------------------------------------------------------------------------
 
-#wget https://github.com/xgi/houdoku/releases/download/v2.11.0/Houdoku-2.11.0.AppImage
-#wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-lite-2.2.0-travis995-0f91801-x86_64.AppImage
 
 echo " Installing Oh my zsh "
 
--- Themes.sh
+++ Themes.sh
@@ -4,20 +4,19 @@ echo "##############################"
 echo "      Cloning themes          "
 echo "##############################"
 
-#git clone https://github.com/EliverLara/Kripton
-#git clone https://github.com/EliverLara/Nordic
-#git clone https://github.com/EliverLara/otis
-#git clone https://github.com/darkomarko42/Quartz-Night
-#git clone https://github.com/vinceliuice/Graphite-gtk-theme
-#git clone https://github.com/i-mint/bluesky
-#git clone https://github.com/vinceliuice/Colloid-gtk-theme
-#git clone https://github.com/vinceliuice/Nephrite-gtk-theme
-#git clone https://github.com/i-mint/LightningBug
-#git clone https://github.com/vinceliuice/Lavanda-gtk-theme
-#git clone https://github.com/erenyldz89/Marshmallow1
-#git clone https://github.com/EliverLara/Sweet
-#git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme
-#git clone https://github.com/ZorinOS/zorin-desktop-themes
+git clone https://github.com/EliverLara/Kripton
+git clone https://github.com/EliverLara/Nordic
+git clone https://github.com/EliverLara/otis
+git clone https://github.com/vinceliuice/Graphite-gtk-theme
+git clone https://github.com/i-mint/bluesky
+git clone https://github.com/vinceliuice/Colloid-gtk-theme
+git clone https://github.com/vinceliuice/Nephrite-gtk-theme
+git clone https://github.com/i-mint/LightningBug
+git clone https://github.com/vinceliuice/Lavanda-gtk-theme
+git clone https://github.com/erenyldz89/Marshmallow1
+git clone https://github.com/EliverLara/Sweet
+git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme
+git clone https://github.com/ZorinOS/zorin-desktop-themes
 
 
 echo "##################################################"
@@ -62,19 +61,6 @@ cd otis
 rm -rf package.json package-lock.json Gulpfile.js Art src
 cd ..
 mv otis ~/.themes
-
-echo "                                         "
-echo "                                         "
-echo "                                         "
-echo "                                         "
-
-echo "##################################################"
-echo "              Adding Quartz-Night                 "
-echo "##################################################"
-
-cd Quartz-Night
-mv Quartz Night ~/.themes
-cd ..
 
 echo "                                         "
 echo "                                         "
