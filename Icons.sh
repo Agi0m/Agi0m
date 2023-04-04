2e899f6db8170aa03e5db57cde0fd0e1f9ab79aa
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-04-04 22:48:41 +0200

    Added icons script ( it hasn't been tested)

--- Icons.sh
+++ Icons.sh
@@ -9,11 +9,9 @@ git clone https://github.com/zayronxio/Mkos-Big-Sur
 git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme
 git clone https://github.com/zayronxio/Deepin-icons-2022
 git clone https://github.com/bikass/kora
-git clone https://github.com/vinceliuice/Mojave-gtk-theme
 git clone https://github.com/luisrguerra/monday-icon-theme
 git clone https://github.com/alvatip/Nordzy-icon
 git clone https://github.com/yeyushengfan258/Reversal-icon-theme
-git clone https://github.com/L4ki/Infinity-Plasma-Themes
 git clone https://github.com/cbrnix/Newaita-reborn
 git clone https://github.com/4e6anenk0/Rowaita-icon-theme
 git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme
@@ -21,8 +19,159 @@ git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme
 
 
 echo "##############################"
-echo "      Installing icons           "
+echo "      Installing icons        "
 echo "##############################"
 
 mkdir ~/.icons
 
+echo "                                           "
+echo "                                           "
+echo "                                           "
+
+
+echo "##############################"
+echo "    Installing candy icons    "
+echo "##############################"
+
+mv candy-icons ~/.icons
+
+
+echo "                                           "
+echo "                                           "
+echo "                                           "
+
+
+echo "##############################"
+echo "    Installing Mkos Big Sur   "
+echo "##############################"
+
+mv Mkos-Big-Sur ~/.icons
+
+echo "                                           "
+echo "                                           "
+echo "                                           "
+
+
+echo "##############################"
+echo "  Installing Catppuccin icons "
+echo "##############################"
+
+cd Catppuccin-GTK-Theme
+cd icons
+mv * ~/.icons
+cd ..
+cd ..
+
+
+echo "                                           "
+echo "                                           "
+echo "                                           "
+
+
+echo "##############################"
+echo "    Installing Deepin icons   "
+echo "##############################"
+
+mv Deepin-icons-2022 ~/.icons
+
+
+echo "                                           "
+echo "                                           "
+echo "                                           "
+
+
+echo "##############################"
+echo "    Installing kora icons     "
+echo "##############################"
+
+cd kora
+mv kora kora-pgrey ~/.icons
+cd .
+
+echo "                                           "
+echo "                                           "
+echo "                                           "
+
+
+echo "##############################"
+echo "    Installing Monday icons   "
+echo "##############################"
+
+cd monday-icon-theme
+mv Monday ~/.icons
+cd ..
+
+echo "                                           "
+echo "                                           "
+echo "                                           "
+
+
+echo "##############################"
+echo "    Installing Nordzy icons   "
+echo "##############################"
+
+cd Nordzy-icon
+mkdir ~/.icons/Nordzy
+cd src
+mv * ~/.icons/Nordzy
+cd ..
+cd ..
+
+
+echo "                                           "
+echo "                                           "
+echo "                                           "
+
+
+echo "##############################"
+echo "  Installing Reversal icons   "
+echo "##############################"
+
+
+cd Reversal-icon-theme
+mkdir Reversal ~/.icons/Reversal
+cd src
+mv * ~/.icons/Reversal
+cd ..
+cd ..
+
+
+echo "                                           "
+echo "                                           "
+echo "                                           "
+
+
+echo "##############################"
+echo "    Installing Newaita icons  "
+echo "##############################"
+
+cd Newaita-reborn
+mv Newaita-reborn-dark Newaita-reborn-mint-dark Newaita-reborn-fedora-dark ~/.icons
+cd ..
+
+echo "                                           "
+echo "                                           "
+echo "                                           "
+
+
+echo "##############################"
+echo "    Installing Rowaita icons  "
+echo "##############################"
+
+cd Rowaita-icon-theme
+mv Rowaita Rowaita-Adw-Dark Rowaita-Black-Dark Rowaita-Nord-Dark ~/.icons
+cd ..
+
+
+echo "                                           "
+echo "                                           "
+echo "                                           "
+
+
+echo "##############################"
+echo " Installing Tokyo Night icons "
+echo "##############################"
+
+cd Tokyo-Night-GTK-Theme
+cd icons
+mv * ~/.icons
