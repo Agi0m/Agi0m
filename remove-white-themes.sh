642c94f9ff41e0528bd54cf7c00a82d54667e5f8
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-04-04 20:57:25 +0200

    Added option to remove white themes at .themes

--- remove-white-themes.sh
+++ remove-white-themes.sh
@@ -8,9 +8,9 @@ echo "############################################"
 
 
 cd ~/.themes
-rm -rf 
+rm -rf BlueSky-light BlueSky-Clean* BlueSky-Mountain-* BlueSky-Solid-Light Colloid-Dark-hdpi Colloid-Dark-xhdpi Colloid-hdpi Colloid-Light* Colloid-xhdpi Graphite-Dark-hdpi Graphite-Dark-xhdpi Graphite-Light* Jasper-Light* Lavanda-Light* Lightningbug-Light* Tokyonight-Light* ZorinBlue-Light ZorinGreen-Light ZorinGrey-Light ZorinOrange-Light ZorinPurple-Light ZorinRed-Light
 
 echo "                                     "
 echo "                                     "
 echo "                                     "
-echo " All done"
+echo " All done "
