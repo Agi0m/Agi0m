cf21e713dcb7424aa138c9aa16c2a90bc9c7f20c
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-04-04 21:52:53 +0200

    Getting ready to create icons script

---
+++ Icons.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+echo "##############################"
+echo "      Cloning icons           "
+echo "##############################"
+
+git clone https://github.com/EliverLara/candy-icons
+git clone https://github.com/zayronxio/Mkos-Big-Sur
+git clone https://github.com/Fausto-Korpsvart/Catppuccin-GTK-Theme
+git clone https://github.com/zayronxio/Deepin-icons-2022
+git clone https://github.com/bikass/kora
+git clone https://github.com/vinceliuice/Mojave-gtk-theme
+git clone https://github.com/luisrguerra/monday-icon-theme
+git clone https://github.com/alvatip/Nordzy-icon
+git clone https://github.com/yeyushengfan258/Reversal-icon-theme
+git clone https://github.com/L4ki/Infinity-Plasma-Themes
+git clone https://github.com/cbrnix/Newaita-reborn
+git clone https://github.com/4e6anenk0/Rowaita-icon-theme
+git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme
+
+
+
+echo "##############################"
+echo "      Installing icons           "
+echo "##############################"
+
+mkdir ~/.icons
+
