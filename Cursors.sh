3229d4cf1f2ece2fb38e18bc629e12c9d82af3ae
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-04-05 10:04:15 +0200

    Added cursors script

---
+++ Cursors.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+echo "##############################"
+echo "      Cloning Cursors         "
+echo "##############################"
+
+echo "                                         "
+echo "                                         "
+echo "                                         "
+echo "                                         "
+
+git clone  https://github.com/alvatip/Borealis-cursors
+git clone https://github.com/alvatip/Nordzy-cursors
+git clone https://github.com/vinceliuice/Colloid-icon-theme/tree/main/cursors
+git clone https://github.com/dracula/gtk/tree/master/kde/cursors
+
+echo "##############################"
+echo "      Installing Cursors      "
+echo "##############################"
+
+echo "                                         "
+echo "                                         "
+echo "                                         "
+echo "                                         "
+
+
