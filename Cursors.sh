d54efeb700ea5c9d80b24ed6db4e05a668bb7f32
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-04-05 10:22:05 +0200

    Updating cursors

--- Cursors.sh
+++ Cursors.sh
@@ -11,11 +11,10 @@ echo "                                         "
 
 git clone  https://github.com/alvatip/Borealis-cursors
 git clone https://github.com/alvatip/Nordzy-cursors
-git clone https://github.com/vinceliuice/Colloid-icon-theme/tree/main/cursors
-git clone https://github.com/dracula/gtk/tree/master/kde/cursors
+git clone https://github.com/vinceliuice/Colloid-icon-theme
 
 echo "##############################"
-echo "      Installing Cursors      "
+echo "Installing Brorealis cursors  "
 echo "##############################"
 
 echo "                                         "
@@ -23,4 +22,30 @@ echo "                                         "
 echo "                                         "
 echo "                                         "
 
+cd Broealis-cursors
+mv Borealis-cursors ./.icons
+cd ..
 
+
+echo "##############################"
+echo "  Installing Nordzy cursors   "
+echo "##############################"
+
+echo "                                         "
+echo "                                         "
+echo "                                         "
+echo "                                         "
+
+cd Nordzy-cursors
+mv Nordzy-cursors Nordzy-cursors-lefthand ./icons
+cd ..
+
+
+echo "##############################"
+echo "Installing Colloid cursors    "
+echo "##############################"
+
+echo "                                         "
+echo "                                         "
+echo "                                         "
+echo "                                         "
