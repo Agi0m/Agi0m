de4760249ea059e521f99500f1971db046d57127
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-04-07 19:09:46 +0200

    Added some themes back

--- 3-remove-some-mint-themes.sh
+++ 3-remove-some-mint-themes.sh
@@ -7,15 +7,14 @@ cd /usr/share/themes
 sudo rm -rf Mint-X
 sudo rm -rf Mint-X-*
 sudo rm -rf Mint-Y
-sudo rm -rf Mint-Y-Aqua Mint-Y-Blue Mint-Y-Brown Mint-Y-Grey Mint-Y-Legacy-* Mint-Y-Purple Mint-Y-Red Mint-Y-Sand 
-
+sudo rm -rf Mint-Y-Aqua Mint-Y-Blue Mint-Y-Brown Mint-Y-Grey Mint-Y-Orange Mint-Y-Pink Mint-Y-Purple Mint-Y-Red Mint-Y-Sand Mint-Y-Teal Mint-Y-Legacy-*
 echo "Done"
 echo "Switching to icons"
 
 cd /usr/share/icons
 
-sudo rm -rf Mint-X Mint-X-*
-sudo rm -rf Mint-Y-Aqua Mint-Y-Blue Mint-Y-Brown Mint-Y-Dark Mint-Y-Legacy-* Mint-Y-Orange Mint-Y-Pink Mint-Y-Red Mint-Y-Sand Mint-Y-Teal 
+sudo rm -rf  Mint-X-*
+sudo rm -rf Mint-Y-Legacy-*
 sudo rm -rf Yaru Yaru-*
 
 echo "All done"
