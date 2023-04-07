0442cd225a25c1a54d4040e7f793d6b7cba5473b
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-04-07 19:40:39 +0200

    Added Numix to remove icons script

--- 3-remove-some-mint-themes.sh
+++ 3-remove-some-mint-themes.sh
@@ -23,6 +23,7 @@ cd /usr/share/icons
 sudo rm -rf  Mint-X-*
 sudo rm -rf Mint-Y-Legacy-* Mint-Y-Aqua Mint-Y-Blue Mint-Y-Brown Mint-Y-Grey Mint-Y-Orange Mint-Y-Pink Mint-Y-Purple Mint-Y-Red Mint-Y-Sand Mint-Y-Teal
 sudo rm -rf Yaru Yaru-*
+sudo rm -rf Numix Numix-*
 
 
 echo "##############################"
