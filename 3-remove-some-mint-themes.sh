1688f008713bebff2f137f2551a47f71a4f6010a
Agi0m <47166722+Agi0m@users.noreply.github.com>
2023-04-07 19:43:19 +0200

    Fixed syntax to match other scripts

--- 3-remove-some-mint-themes.sh
+++ 3-remove-some-mint-themes.sh
@@ -15,8 +15,24 @@ sudo rm -rf Mint-X
 sudo rm -rf Mint-X-*
 sudo rm -rf Mint-Y
 sudo rm -rf Mint-Y-Aqua Mint-Y-Blue Mint-Y-Brown Mint-Y-Grey Mint-Y-Orange Mint-Y-Pink Mint-Y-Purple Mint-Y-Red Mint-Y-Sand Mint-Y-Teal Mint-Y-Legacy-*
-echo "Done"
-echo "Switching to icons"
+
+echo "##############################"
+echo "           Done               "
+echo "##############################"
+
+echo "                                         "
+echo "                                         "
+echo "                                         "
+echo "                                         "
+
+echo "##############################"
+echo "      Switching to icons      "
+echo "##############################"
+
+echo "                                         "
+echo "                                         "
+echo "                                         "
+echo "                                         "
 
 cd /usr/share/icons
 
